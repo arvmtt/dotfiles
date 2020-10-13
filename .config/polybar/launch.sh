2c6d1ac9adb1c3b4a5f9839645020e66ad38b7e1
arvmtt <arvmtt@arch.localdomain>
2020-10-13 15:40:52 +0200

    feat(polybar): add configuration file and launch script

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#   POLYBAR LAUNCH CONFIGURATION
+#   @arvmtt
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+polybar main -c ~/.config/polybar/config &
+
